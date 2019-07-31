8c6ef1d431c076ba3c2e4b5cbd52481b78c320d0
moonspam <booboo0412@gmail.com>
2019-07-31 16:17:38 +0900

    ⏱️

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,142 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="nes-container with-title is-centered">
+      <h1 class="title">{{ title }}</h1>
+      <i class="nes-ash"></i>
+      <div class="num">
+        <span>{{ minutes }}</span>
+        <span>:</span>
+        <span>{{ seconds }}</span>
+      </div>
+      <div class="btns">
+        <button class="nes-btn" v-if="timer" @click="stopTimer">Pause</button>
+        <button class="nes-btn is-primary" v-else @click="startTimer">PLAY</button>
+        <button class="nes-btn is-error" v-if="resetButton" @click="resetTimer">R</button>
+        <button class="nes-btn is-success" @click="toggleFullScreen">F</button>
+      </div>
+    </div>
+
+    <dialog class="nes-dialog is-rounded" id="dialog-rounded">
+      <form method="dialog">
+        <p class="title">Alert</p>
+        <p>Return to your seat.</p>
+        <menu class="dialog-menu">
+          <button class="nes-btn is-primary">Confirm</button>
+        </menu>
+      </form>
+    </dialog>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+const setTime = 15;
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      title: 'TIMER',
+      timer: null,
+      totalTime: setTime * 60,
+      resetButton: false,
+    };
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => this.countdown(), 1000);
+      this.resetButton = true;
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.timer = null;
+      this.resetButton = true;
+    },
+    resetTimer() {
+      this.totalTime = setTime * 60;
+      clearInterval(this.timer);
+      this.timer = null;
+      this.resetButton = false;
+    },
+    countdown() {
+      if (this.totalTime >= 1) {
+        this.totalTime -= 1;
+      } else {
+        this.totalTime = 0;
+        clearInterval(this.timer);
+        this.resetTimer();
+        document.getElementById('dialog-rounded').showModal();
+        // eslint-disable-next-line
+        // alert('END');
+      }
+    },
+    pad(time) {
+      return (time < 10 ? '0' : '') + time;
+    },
+    toggleFullScreen() {
+      const doc = window.document;
+      const docEl = doc.documentElement;
+      const requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen
+        || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+      const cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen
+        || doc.webkitExitFullscreen || doc.msExitFullscreen;
+      if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement
+        && !doc.msFullscreenElement) {
+        requestFullScreen.call(docEl);
+      } else {
+        cancelFullScreen.call(doc);
+      }
+    },
+  },
+  computed: {
+    minutes() {
+      const minutes = Math.floor(this.totalTime / 60);
+      return this.pad(minutes);
+    },
+    seconds() {
+      const seconds = this.totalTime - (this.minutes * 60);
+      return this.pad(seconds);
+    },
   },
 };
 </script>
 
 <style lang="scss">
+html, body, pre, code, kbd, samp {
+  font-family: 'Press Start 2P', cursive;
+}
+
+menu {
+  padding: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000;
+  > div {
+    width: 320px;
+  }
+  .num {
+    padding: 10px 0;
+    font-size: 48px;
+  }
+  .btns {
+    display: flex;
+    button {
+      flex: 1;
+    }
+    .is-error {
+      flex: none;
+      width: 50px;
+    }
+    .is-success {
+      flex: none;
+      margin-left: 10px;
+      width: 50px;
+    }
+  }
 }
 </style>
